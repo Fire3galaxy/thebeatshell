6636476f66d4d4b2729878d65985eaccff12dd0e
Daniel Handojo <dhand002@hammer.cs.ucr.edu>
2015-01-23 17:52:00 -0800

    Completed rshell.cpp

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -11,30 +11,35 @@ using namespace boost;
 using namespace std;
 
 int main() {
-	cout << "$ " << flush;
-	
-	string command = "";
-	getline(cin, command);	// Get command
+	while (true) {
+		cout << "$ " << flush;
+		
+		string command = "";
+		getline(cin, command);	// Get command
 
-	comParse cp;
-	char* const* argv = cp.parseLine(command); // Parse line
+		comParse cp;
+		char* const* argv = cp.parseLine(command); // Parse line
 
-	int pid = fork();
-	if (pid == -1) {	// Error in fork
-		perror("error in fork");
-		exit(-1);
-	} else if (pid == 0) {	// Child Process
-		if (-1 == execvp(argv[0], argv)) {
-			perror("error in execvp");
+		char ex[5] = {'e','x','i','t','\0'};
+		if (argv[0] == ex && cp.size() == 1) return 0;
+
+		int pid = fork();
+		if (pid == -1) {	// Error in fork
+			perror("error in fork");
+			exit(-1);
+		} else if (pid == 0) {	// Child Process
+			if (-1 == execvp(argv[0], argv)) {
+				perror("error in execvp");
+				exit(-1);
+			}
+
+			exit(1); // should never happen, since child should exec
+		} 
+		
+		if (-1 == wait(0)) {	// Parent Process
+			perror("error in wait");
 			exit(-1);
 		}
-
-		exit(1); // should never happen, since child should exec
-	} 
-	
-	if (-1 == wait(0)) {	// Parent Process
-		perror("error in wait");
-		exit(-1);
 	}
 
 	return 0;
