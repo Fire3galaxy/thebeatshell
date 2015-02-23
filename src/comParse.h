8b73fd10c9f6c25dd55137bd5471cac518e168a0
Iron-Sky-5 <pokeforce72@gmail.com>
2015-02-22 16:41:49 -0800

    Changed parsing for Append Op

--- src/comParse.h
+++ src/comParse.h
@@ -38,7 +38,7 @@ public:
 					exit(-1);
 				}
 				
-				if (inpInd - 1 < 0 || isdigit(com.at(inpInd - 1))) {
+				if (!words.empty() && (inpInd - 1 < 0 || isdigit(com.at(inpInd - 1)))) {
 					if (inpInd - 2 < 0  || isspace(com.at(inpInd - 2)))
 						words.back() += *it;
 					else words.push_back(*it);
@@ -55,10 +55,12 @@ public:
 					exit(-1);
 				}
 				
-				if (inpInd - 1 < 0 || isdigit(com.at(inpInd - 1))) {
-					if (inpInd - 2 < 0  || isspace(com.at(inpInd - 2)))
+				if (!words.empty() && (inpind - 1 < 0 || isdigit(com.at(inpind - 1)))) { 	// #> syntax
+					if (inpind - 2 < 0  || isspace(com.at(inpind - 2)))
 						words.back() += *it;
 					else words.push_back(*it);
+				} else if (!words.empty() && inpind - 1 >= 0 && com.at(inpind - 1) == '>') {	// >> syntax
+					words.back() += *it;
 				} else words.push_back(*it);
 
 				countOR++;
